36b0951418656762420d6da45d89ad2e3ef80964
chaillanYoan <yoanchaillan@yahoo.fr>
2017-12-29 19:09:44 +0100

    navbar dans index.html

--- js/menuListener.js
+++ js/menuListener.js
@@ -1,6 +1,11 @@
 $(function() {
     $('#home').click(function() {
+        //chargement du contenu de la pge
         $('#main').load('game.html');
+
+        //changement du lien actif
+        $('li,#home').addClass('active');
+        $('li,#links').removeClass('active');
         return false;
     });
 });
@@ -8,6 +13,8 @@ $(function() {
 $(function() {
     $('#links').click(function() {
         $('#main').load('options.html');
+        $('li,#links').addClass('active');
+        $('li,#home').removeClass('active');
         return false;
     });
 });
